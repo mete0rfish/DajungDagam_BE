125acf6d1e6c9cef43da1363db791327fc67df12
mereorfish <63222221+mete0rfish@users.noreply.github.com>
2023-12-22 17:06:35 +0900

    init commit

---
+++ djdg/src/main/java/com/dajeongdagam/djdg/DjdgApplication.java
@@ -0,0 +1,13 @@
+package com.dajeongdagam.djdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjdgApplication.class, args);
+	}
+
+}
---
+++ djdg/src/test/java/com/dajeongdagam/djdg/DjdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dajeongdagam.djdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
